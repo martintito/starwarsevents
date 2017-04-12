ca1d56fa86c92f9eaf6dd7398a97b427073851d3
martintito <martin.tito@hotmail.com>
2017-04-12 00:17:30 -0500

    Modificando variable 'name' en DefaultController

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($lastname)
     {
-        return $this->render('YodaEventBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('YodaEventBundle:Default:index.html.twig', array('lastname' => $lastname));
     }
 }
