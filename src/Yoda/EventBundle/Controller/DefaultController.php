47cf708f6d3fae91f0d9c82bb58827973f586f7e
martintito <martin.tito@hotmail.com>
2017-04-12 18:28:57 -0500

    commit inicial en repo local de prensmart

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($lastname)
+    public function indexAction($name)
     {
-        return $this->render('YodaEventBundle:Default:index.html.twig', array('lastname' => $lastname));
+        return $this->render('YodaEventBundle:Default:index.html.twig', array('lastname' => $name));
     }
 }
