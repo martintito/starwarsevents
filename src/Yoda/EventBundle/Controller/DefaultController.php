bc15e1cb345d0f60410ad4d6b6bd7b55a68a98e0
martintito <martin.tito@hotmail.com>
2017-04-13 13:42:56 -0500

    Routing/Controllers

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,11 +3,36 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($count, $firstname)
     {
-        return $this->render('YodaEventBundle:Default:index.html.twig', array('lastname' => $name));
+        //return $this->render('YodaEventBundle:Default:index.html.twig', array('lastname' => $name));
+        //return new Response('It\'s a traaaaaaap!');
+        /*$data = array(
+            'count' => $count,
+            'firstname' => $firstname,
+            'ackbar' => 'It\'s a traaaaaaap!'            
+        );
+        $json = json_encode($data);
+        
+        $response = new Response($json);
+        $response->headers->set('Content-Type', 'application/json');
+        
+        return $response;*/
+        
+        /*$templating = $this->container->get('templating');
+        
+        return $templating->renderResponse(
+                'YodaEventBundle:Default:index.html.twig',
+                array('name' => $firstname)
+                );*/
+        
+        return $this->render(
+                'YodaEventBundle:Default:index.html.twig',
+                array('name' => $firstname)
+                );
     }
 }
