e0d2e95779b5a4ce5b22af445e79e05db91da47e
martintito <martin.tito@hotmail.com>
2017-04-27 00:28:07 -0500

    FOSUserBundle

---
+++ src/Yoda/EventBundle/Entity/User.php
@@ -0,0 +1,25 @@
+<?php 
+namespace Yoda\EventBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM; 
+use FOS\UserBundle\Model\User as BaseUser; 
+/** 
+ * 
+ * @ORM\Table(name="fos_user") 
+ * @ORM\Entity 
+ */ 
+class User extends BaseUser 
+{ 
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+} 
